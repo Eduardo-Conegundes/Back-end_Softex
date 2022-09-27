a4dbc08148f7c0f9c5c74bb6e365f5f5bcf04718
Luiz Eduardo <duduconegundes@gmail.com>
2022-09-27 08:42:51 -0300

    Update questao1.py

--- Módulo 2 - Introdução a Programação/questao1.py
+++ Módulo 2 - Introdução a Programação/questao1.py
@@ -1,1 +1,12 @@
-print("Hello World")
+nome = (input("Digite a idade do aluno(a): "))
+n1 = float(input("Digite a primeira nota do aluno(a): "))
+n2 = float(input("Digite a segunda nota do aluno(a): "))
+faltas = int(input("Digite a quantidade de faltas do aluno(a): "))
+
+media = (n1+n2)/2
+
+if(media >=7 and faltas < 3):
+    print("Aluno aprovado com média " , media , " e " , faltas , "faltas")
+else:
+    print("Aluno reprovado com média " , media , " e " , faltas , "faltas")
+
