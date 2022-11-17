b1a2cc99826e3e813560bee1a2eccb14de9ffdfb
Luiz Eduardo <duduconegundes@gmail.com>
2022-11-17 07:56:59 -0300

    Create questao15.java

---
+++ Módulo 2 - Introdução a Programação/questao15.java
@@ -0,0 +1,20 @@
+package.br.com.empresa.cliente;
+
+public class cadastro{
+
+}
+
+public class atualizar{
+
+}
+
+//novo pacote
+package.br.com.empresa.vendedor;
+
+public class meta_venda{
+
+}
+
+public class cadastro{
+    
+}
