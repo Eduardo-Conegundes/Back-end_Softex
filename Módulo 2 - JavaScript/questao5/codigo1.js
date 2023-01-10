12e635cdf83aef3d702de1d9947d22396293e3ff
Luiz Eduardo <duduconegundes@gmail.com>
2023-01-10 10:42:07 -0300

    Create codigo1.js

---
+++ Módulo 2 - JavaScript/questao5/codigo1.js
@@ -0,0 +1,16 @@
+Aqui está um exemplo de como você poderia criar um programa em JavaScript que informa se um aluno foi reprovado ou não com base em três notas usando um operador de atribuição e um operador ternário:
+
+Copy code
+// Obtém as três notas do aluno
+let grade1 = parseFloat(prompt("Informe a nota 1: "));
+let grade2 = parseFloat(prompt("Informe a nota 2: "));
+let grade3 = parseFloat(prompt("Informe a nota 3: "));
+
+// Calcula a média das três notas
+let mean = (grade1 + grade2 + grade3) / 3;
+
+// Utiliza o operador de atribuição para definir a variável "result" como "Aprovado" ou "Reprovado"
+let result = mean >= 6 ? "Aprovado" : "Reprovado";
+
+console.log("Média: " + mean);
+console.log("Resultado: " + result);
