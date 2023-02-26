9fa9c849726974a7ff4d6511ac74e9350a2a7179
Luiz Eduardo <duduconegundes@gmail.com>
2023-02-26 18:37:09 -0300

    Delete rota.js

--- Módulo 2 - JavaScript/rota.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express();
-
-app.get('/AnimesOnline', (request, response) => {
-    return response.send("Hello World");
-});
-
-app.listen(777);
-console.log("Rodando...")
